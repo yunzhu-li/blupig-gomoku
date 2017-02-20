5a7d5d57ca94366c8fb786b169320d9154035b26
Yunzhu Li <contact@yunzhu.li>
2017-02-20 02:38:25 -0500

    Update docker start script

--- docker-start.sh
+++ docker-start.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 # Replace server uri and path
 sed -i "s|var api_base_url.*|var api_base_url = '$SERVER_URI';|" /app/gui/client/index.html
 
+# Start nginx daemon
 nginx
+
+# Start node server
 cd /app/gui/server
-npm start
+exec npm start
